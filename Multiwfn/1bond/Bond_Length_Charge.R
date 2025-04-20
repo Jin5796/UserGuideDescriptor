17fb10a1380d753d6e3bf4ca8e1a4fec0f8fb259
Jin5796 <2119105560@qq.com>
2025-04-20 17:37:58 +0800

    Bond_Length_Charge.R

--- Multiwfn/1bond/Bond_Length_Charge.R
+++ Multiwfn/1bond/Bond_Length_Charge.R
@@ -1,4 +1,4 @@
-# -2 to -1 出错
+
 # Collect Bond length Charge
 
 # Clear all variables
@@ -10,7 +10,7 @@ library(stringr)
 library(plyr)
 
 # List chg files from directory, extract chemical name
-data=dir(path="F:/2ndALL/0 Gaussian16计算文件/HF_N_fchk/ADCH Charges/", pattern="\\.chg")
+data=dir(path="F:/2ndALL/0 Gaussian16/HF_N_fchk/ADCH Charges/", pattern="\\.chg")
 
 # Remove extension, leading p
 data=gsub(".chg","",data)
@@ -25,10 +25,10 @@ df$Radialpo=NA
 df$Angularpo=NA
 df$FBOmax=NA
 df$FBOmin=NA
-df$FBOmin5=NA #补充Bond>5min
+df$FBOmin5=NA 
 
 df$QCPositivemax=NA
-df$QCNegativemax=NA #补充C-max
+df$QCNegativemax=NA
 df$QCNegativemin=NA
 df$QHPositivemax=NA
 
@@ -40,8 +40,8 @@ df$LccAver=NA
 for (i in 1:length(chemical)){
   
   #     Scan in each chg and txt file
-  fpchg <- paste("F:/2ndALL/0 Gaussian16计算文件/HF_N_fchk/ADCH Charges/p", chemical[i], ".chg", sep="")
-  fptxt <- paste("F:/2ndALL/0 Gaussian16计算文件/HF_N_fchk/Bond_Length/p", chemical[i], "_Bond_Length_Chargenouse.txt", sep="")  
+  fpchg <- paste("F:/2ndALL/0 Gaussian16/HF_N_fchk/ADCH Charges/p", chemical[i], ".chg", sep="")
+  fptxt <- paste("F:/2ndALL/0 Gaussian16/HF_N_fchk/Bond_Length/p", chemical[i], "_Bond_Length_Chargenouse.txt", sep="")  
   
   chgfile <- scan(file=fpchg, what=character(), sep="\n")
   txtfile <- scan(file=fptxt, what=character(), sep="\n")
@@ -69,14 +69,14 @@ for (i in 1:length(chemical)){
   Bondorder1 = Bondorder1+1
   Bondorder2 = grep("If outputting bond order matrix to bndmat", txtfile)
   Bondorder2 = Bondorder2-2
-  # 做for循环前，一定要先定义变量
+  
   BondOrder =NULL
   BondOrder5 =NULL
   k = 0
   for (j in Bondorder1:Bondorder2){
     BondOrder[j] = as.numeric(gsub(x=str_sub(string=txtfile[j], start=-10), pattern=" ", replacement=""))     
     
-	#单独提取出BondOrder>0.5的Bond order为一个向量
+	
 	if (BondOrder[j]>0.5){
 	BondOrder5[k] = BondOrder[j]
 	k = k + 1}
@@ -98,12 +98,12 @@ for (i in 1:length(chemical)){
   for (j in C){
     Qc[j] = as.numeric(gsub(x=str_sub(string=chgfile[j], start=-13), pattern=" ", replacement=""))     
 	
-	#单独提取出Qc<0的电荷为一个向量
+	
 	if (Qc[j]<0){
 	QcNegative[m] = Qc[j]
 	m = m + 1}
 	}
-	df$QCNegativemax[rindx] = max(QcNegative)#如果向量只有一个值，不会输出，会显示=inf
+	df$QCNegativemax[rindx] = max(QcNegative)
 	
   for (j in H){
     Qh[j] = as.numeric(gsub(x=str_sub(string=chgfile[j], start=-13), pattern=" ", replacement=""))     
@@ -128,4 +128,4 @@ for (i in 1:length(chemical)){
 	
 	
 }
-write.csv(df, file="F:/2ndALL/0 Gaussian16计算文件_替换2分子/Chg_txt_bond_length_charge_173_to.csv", row.names=F)
+write.csv(df, file="F:/2ndALL/0 Gaussian16/Chg_txt_bond_length_charge_173_to.csv", row.names=F)
